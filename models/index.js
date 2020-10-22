48cbb15c9f492cc469972efbb0fb52cf4f5a3a7a
claire p <cpuckett9@gmail.com>
2020-10-21 20:11:51 -0600

    completed post routes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,14 @@
 const User = require('./User');
-const Post = require("./Post")
+const Post = require("./Post");
+
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+
+  //user can have many posts, but post can only have 1 user
+  Post.belongsTo(User, {
+    foreignKey: 'user_id',
+  });
 
 module.exports = { User, Post };
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const postRoutes = require('./api/post-routes');
+
 
 router.use('/api', apiRoutes);
+router.use('/posts', postRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
