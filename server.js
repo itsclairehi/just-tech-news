55a02fa798bdd34a85f1c39d64882d6db5b549c0
claire p <cpuckett9@gmail.com>
2020-11-21 15:09:12 -0600

    impemented patials and helper functions

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 //handlebars setup
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+//helper functions
+const helpers = require('./utils/helpers');
+//pass helpers to handlebars
+const hbs = exphbs.create({ helpers });
 
 //session setup
 const session = require('express-session');
